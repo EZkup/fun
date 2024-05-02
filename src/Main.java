97261036b51c3e547908c5779fa4232c6f032fec
Teacher <Denrip2018@gmail.com>
2024-05-02 16:20:35 +0700

    fun

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       printname (9);
+       System.out.println("/////////////////////////");
+       printname (7);
+
+    }
+
+    public static void printname(int NUM) {
+        System.out.println(NUM+NUM);
+        System.out.println(NUM-NUM);
+        System.out.println(NUM*NUM);
+        System.out.println(NUM/NUM);
+    }
+}
